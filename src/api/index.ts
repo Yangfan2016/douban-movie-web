458d83eb1be33b846c2a6817b744f62fb6ef2eac
yangfan2016 <15234408101@163.com>
2019-04-08 22:08:46 +0800

    add(ui): ant-design

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,34 @@
 import axios from "axios";
 
+const BASE_URL = "/api/movie";
+const API_KEY = "0b2bdeda43b5688921839c8ecb20399b";
 
-const BASE_URL="/api";
-let instance=axios.create({
-    baseURL:BASE_URL,
-    timeout:3e4,
+let instance = axios.create({
+    baseURL: BASE_URL,
+    timeout: 3e4,
 });
 
 // 热映
 export function getHotShowing() {
-    return instance.get("/movie/in_theaters");
+    return instance.get("/in_theaters");
+}
+
+// top250
+export function getTop250() {
+    return instance.get("/top250");
+}
+
+// 新片
+export function getNew() {
+    let params = {
+        "apikey": API_KEY,
+    };
+    return instance.get("/new_movies", {
+        params,
+    });
+}
+
+// 电影详情
+export function getDetail(id: string) {
+    return instance.get(id);
 }
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,10 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(proxy('/api', {
-    target: 'http://api.douban.com/v2',
+    target: 'http://api.douban.com/',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/api': '/v2', // 重写路径
+    },
   }));
 };
