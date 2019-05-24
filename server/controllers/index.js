e91cb3743612252437896f79a815e44bb2bb545c
yangfan2016 <15234408101@163.com>
2019-05-24 22:52:09 +0800

    add: server

---
+++ server/controllers/index.js
@@ -0,0 +1,45 @@
+const Koa = require("koa");
+const Router = require("koa-router");
+const proxy = require("koa-server-http-proxy");
+const static = require("koa-static");
+const fs = require("fs");
+const path = require("path");
+
+const router = Router();
+const app = new Koa;
+
+const PORT = 9876;
+
+
+// views
+router.get("/", async (ctx, next) => {
+  let str = fs.readFileSync(path.resolve(__dirname, "../build/index.html"), "utf-8");
+  ctx.response.body = str;
+});
+
+// 404
+// router.get("*", async (ctx, next) => {
+//   ctx.response.redirect("/");
+// })
+
+app.use(proxy('/api', {
+  target: 'http://api.douban.com/',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api': '/v2', // 重写路径
+  },
+}));
+app.use(proxy('/bing', {
+  target: 'https://www.bing.com/',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/bing': '/', // 重写路径
+  },
+}));
+app.use(static(__dirname, "../build"));
+app.use(router.routes());
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
+
